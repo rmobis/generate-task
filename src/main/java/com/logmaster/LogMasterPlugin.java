b02c6ad18a9c176ebe903fa3ec5ea4f969aba827
Raphael Mobis Tacla <r.mobis@gmail.com>
2025-07-18 07:29:29 -0300

    cleanup unused code and remove deprecated InterfaceID class

--- src/main/java/com/logmaster/LogMasterPlugin.java
+++ src/main/java/com/logmaster/LogMasterPlugin.java
@@ -4,7 +4,6 @@ import com.google.inject.Provides;
 import com.logmaster.domain.Task;
 import com.logmaster.domain.TaskPointer;
 import com.logmaster.domain.TaskTier;
-import com.logmaster.domain.TieredTaskList;
 import com.logmaster.persistence.SaveDataManager;
 import com.logmaster.task.TaskService;
 import com.logmaster.ui.InterfaceManager;
@@ -14,56 +13,33 @@ import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.SoundEffectID;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.GameTick;
-import net.runelite.api.events.WidgetClosed;
-import net.runelite.api.events.WidgetLoaded;
-import net.runelite.api.widgets.InterfaceID;
+import net.runelite.api.events.*;
+import net.runelite.api.gameval.InterfaceID;
 import net.runelite.api.widgets.Widget;
-import net.runelite.client.callback.ClientThread;
-import net.runelite.client.chat.ChatCommandManager;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
-import net.runelite.client.game.ItemManager;
-import net.runelite.client.game.SpriteManager;
 import net.runelite.client.input.MouseManager;
-import net.runelite.client.menus.MenuManager;
-import net.runelite.client.menus.WidgetMenuOption;
-import net.runelite.api.events.ScriptPostFired;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.client.util.LinkBrowser;
 
 import javax.inject.Inject;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseWheelEvent;
-import java.io.File;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
-@PluginDescriptor(
-	name = "Collection Log Master"
-)
+@PluginDescriptor(name = "Collection Log Master")
 public class LogMasterPlugin extends Plugin {
-	private static final String TASK_CHAT_COMMAND = "!tasker";
-
     private static final int COLLECTION_LOG_SETUP_SCRIPT_ID = 7797;
 
 	@Inject
 	private Client client;
 
 	@Inject
-	private ClientThread clientThread;
-
-	@Inject
 	private LogMasterConfig config;
-
-
-	@Inject
-	private SpriteManager spriteManager;
 
 	@Inject
 	private MouseManager mouseManager;
@@ -75,9 +51,6 @@ public class LogMasterPlugin extends Plugin {
 	private OverlayManager overlayManager;
 
 	@Inject
-	private ChatCommandManager chatCommandManager;
-
-	@Inject
 	private TaskService taskService;
 
 	@Inject
@@ -86,15 +59,8 @@ public class LogMasterPlugin extends Plugin {
 	@Inject
 	private InterfaceManager interfaceManager;
 
-	@Inject
-	private ItemManager itemManager;
-
-	private Map<Integer, Integer> chatSpriteMap = new HashMap<>();
-
-	private File playerFile;
-
 	@Override
-	protected void startUp() throws Exception
+	protected void startUp()
 	{
 		mouseManager.registerMouseWheelListener(interfaceManager);
 		mouseManager.registerMouseListener(interfaceManager);
@@ -102,13 +68,10 @@ public class LogMasterPlugin extends Plugin {
 		this.taskOverlay.setResizable(true);
 		this.overlayManager.add(this.taskOverlay);
 		this.taskService.getTaskList();
-		// TODO when task save data can be stored and access externally; populate this with other people's data
-//		this.clientThread.invoke(this::populateChatSpriteMap);
-//		chatCommandManager.registerCommandAsync(TASK_CHAT_COMMAND, this::getTaskCommandData);
 	}
 
 	@Override
-	protected void shutDown() throws Exception {
+	protected void shutDown() {
 		mouseManager.unregisterMouseWheelListener(interfaceManager);
 		mouseManager.unregisterMouseListener(interfaceManager);
 		this.overlayManager.remove(this.taskOverlay);
@@ -133,14 +96,14 @@ public class LogMasterPlugin extends Plugin {
 
 	@Subscribe
 	public void onWidgetLoaded(WidgetLoaded e) {
-		if(e.getGroupId() == InterfaceID.COLLECTION_LOG) {
+		if(e.getGroupId() == InterfaceID.COLLECTION) {
 			interfaceManager.handleCollectionLogOpen();
 		}
 	}
 
 	@Subscribe
 	public void onWidgetClosed(WidgetClosed e) {
-		if(e.getGroupId() == InterfaceID.COLLECTION_LOG) {
+		if(e.getGroupId() == InterfaceID.COLLECTION) {
 			interfaceManager.handleCollectionLogClose();
 		}
 	}
@@ -257,7 +220,6 @@ public class LogMasterPlugin extends Plugin {
 	public void playFailSound() {
 		client.playSoundEffect(2277);
 	}
-
 
 	@Provides
 	LogMasterConfig provideConfig(ConfigManager configManager)
