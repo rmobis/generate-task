83ed8169cacf6ba46f2356b8c89085192e3a8955
RedSparr0w <RedSparr0w@users.noreply.github.com>
2025-07-20 17:55:13 +1200

    Update LogMasterPlugin.java

--- src/main/java/com/logmaster/LogMasterPlugin.java
+++ src/main/java/com/logmaster/LogMasterPlugin.java
@@ -1,7 +1,5 @@
 package com.logmaster;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonParseException;
 import com.google.inject.Provides;
 import com.logmaster.clog.ClogItemsManager;
 import com.logmaster.domain.Task;
@@ -14,10 +12,7 @@ import com.logmaster.ui.component.TaskOverlay;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.EnumComposition;
 import net.runelite.api.GameState;
-import net.runelite.api.MenuAction;
-import net.runelite.api.Skill;
 import net.runelite.api.SoundEffectID;
 import net.runelite.api.events.*;
 import net.runelite.api.gameval.InterfaceID;
